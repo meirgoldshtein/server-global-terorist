cf33134a9b9647f0c2e003e4f5a964813f0efcc5
meirgoldshtein <m0548491057@gmail.com>
2024-12-18 16:25:02 +0200

    added main app page

---
+++ src/app.ts
@@ -0,0 +1,33 @@
+
+import express, { Request, Response} from 'express';
+import cors from 'cors';
+import http from 'http';
+import { Server } from 'socket.io';
+import {connectDB} from './DAL/mongoConnect';
+import analysis from './routers/analysisRouter';
+import relationships from './routers/relationshipsRouter';
+const PORT = process.env.PORT
+
+export const app = express();
+export const server = http.createServer(app);
+
+app.use(cors());
+app.use(express.json());
+connectDB();
+app.get('/ping', (req: Request, res: Response) => {
+    res.status(200).send('pong')
+})
+
+app.use('/api/analysis', analysis);
+app.use('/api/relationships', relationships);
+
+export const io = new Server(server,{ cors: { origin: "*" } });
+io.on('connection', (socket) => {
+    console.log('socket: a user connected');
+    
+    socket.on('disconnect', () => {
+        console.log('socket: a user disconnected');
+    });
+});
+
+server.listen(PORT, () => console.log(`Listening on port ${PORT},visit http://localhost:${PORT}`));
